27249d26b36045d54e3396e3321586120b727403
renatormc <renatomartinsrmc@gmail.com>
2022-01-14 18:30:44 -0300

    Normal

--- helpers.py
+++ helpers.py
@@ -6,7 +6,8 @@ import config
 def compile(path):
     path = Path(path)
     output = path.parent / f"{path.stem}_ui.py"
-    args = ["python", "-m", "PyQt5.uic.pyuic", str(path), "-o", str(output)]
+    # args = ["python", "-m", "PyQt5.uic.pyuic", str(path), "-o", str(output)]
+    args = ["pyuic5", str(path), "-o", str(output)]
     subprocess.run(args)
 
 def find_ui_files(folder: Path, depth=0) -> List[Path]:
