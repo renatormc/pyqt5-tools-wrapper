ee99cdbd69e860e9f22ec3ff03799ed2e6bc5125
renatormc <renatomartinsrmc@gmail.com>
2022-04-02 15:56:01 -0300

    Add depth param on command line

--- cmd.py
+++ cmd.py
@@ -1,10 +1,12 @@
 import argparse
 import actions as ac
+import config
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(dest="command", required=True, help='Pyqt5 tools')
 
 p_build = subparsers.add_parser("build")
+p_build.add_argument("--depth", type=int, default=5, help="Depth of the search for ui files.")
 
 
 p_new = subparsers.add_parser("new")
@@ -16,6 +18,7 @@ p_designer.add_argument("item",nargs='?')
 
 args = parser.parse_args()
 if args.command == "build": 
+    config.max_depth = args.depth
     ac.build()
 elif args.command == "new":
     ac.new_item(args.item, args.name)
